40c87779a2906b344566b458a9f692a76e44e4e1
Godwin Duliente <rualgodwin@gmail.com>
2022-10-26 11:43:25 +0800

    color theme incomplete

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
-// Theme Click Module
+// Theme Pop Up Module 
 const openTheme = document.getElementById("openTheme")
 const closeTheme = document.getElementById("closeTheme")
 const themeModal = document.getElementById("theme")
 
-openChat.addEventListener('click', () => {
+openTheme.addEventListener('click', () => {
   themeModal.classList.add('show');
 });
 
-closeChat.addEventListener('click', () => {
+closeTheme.addEventListener('click', () => {
   themeModal.classList.remove('show');
 });
+
+// Theme Click Module
+const summerToggle = document.getElementById("summer")
+const fallToggle = document.getElementById("fall")
+const springToggle = document.getElementById("spring")
+const autumnToggle = document.getElementById("autumn")
+const darkToggle = document.getElementById("dark")
+const lightToggle = document.getElementById("light")
+
+
+// Summer Click Module
+summerToggle.addEventListener('click', () => { 
+  document.body.classList.toggle("summer-theme")
+})
+
+// Fall Click Module
+fallToggle.addEventListener('click', () => { 
+  document.body.classList.toggle("fall-theme")
+})
+
+// Spring Click Module
+springToggle.addEventListener('click', () => { 
+  document.body.classList.toggle("spring-theme")
+})
+
+// Autumn Click Module
+autumnToggle.addEventListener('click', () => { 
+  document.body.classList.toggle("autumn-theme")
+})
+
+// Dark Click Module
+darkToggle.addEventListener('click', () => { 
+  document.body.classList.toggle("dark-theme")
+})
+
+// Light Click Module
+lightToggle.addEventListener('click', () => { 
+  document.body.classList.toggle("light-theme")
+})
